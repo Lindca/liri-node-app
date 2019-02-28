6ae1f7c20615a97163fdeeb1d1578ba8c31e1fb4
Lindca <45475418+Lindca@users.noreply.github.com>
2019-02-27 19:17:45 -0800

    done, I should have pushed earlier

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # liri-node-app
+
+liri-node-app
+This program is an app that 
+
+Getting Started:
+Clone my repository to your computer and open ypour terminal then enter in "node liri.js" then a search type of your choosing based on what you want to search for. The syntax is as follows: "node liri.js 'search type' 'keyword'". Your search options are "spotify-this-song"  to search for songs, "conert-this" to search for concerts, and "movie-this" to search for movies. You can also enter "do-what-it-says" as the search type to run the random.txt file and 
+
+Prerequisites:
+You need node installed on your computer.
+
+Download:
+You should be able to run my program just by downloading the files given.
+
+Built With:
+Jquery - The js framework used
+Axios - Axios is a promise based HTTP request library
+ROME - Used to generate RSS Feeds
+Contributing
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+Authors:
+Lindsay Campbell - All content - Freelance Developer
+
---
+++ liri.js
@@ -0,0 +1,96 @@
+
+var axios = require("axios");
+var app = process.argv;
+var searchCriteria = encodeURI(app[3]);
+var moment = require('moment');
+require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+if (app[2] === "spotify-this-song") {
+    searchSpotify(searchCriteria);
+}
+else if (app[2] === "concert-this") {
+    searchConcert(searchCriteria);
+}
+else if (app[2] === "movie-this") {
+    searchMovies(searchCriteria);
+}
+else if (app[2] === "do-what-it-says") {
+    readLog();
+}
+else {
+    console.log("Please try again!");
+    console.log("You may have entered something incorrect or nothing at all!")
+}
+
+function readLog() {
+    fs.readFile('random.txt', "utf8", function (err, data) {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            var dataArray = data.split(",");
+            if (dataArray[0] === 'spotify-this-song') {
+                searchSpotify(dataArray[1])
+            }
+        }
+
+    })
+}
+function searchConcert(searchCriteria) {
+    axios.get("https://rest.bandsintown.com/artists/" + searchCriteria + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            var venues = response.data;
+            // console.log(response.data);
+            for (var i = 0; i < venues.length; i++) {
+                console.log("Venue Name: " + venues[i].venue.name);
+                console.log("Location: " + venues[i].venue.city);
+                console.log("Event Date: " + moment(venues[i].datetime).format("MM/DD/YYYY"));
+            }
+        }
+    );
+}
+function searchMovies(searchCriteria = "Mr. Nobody") {
+    axios.get("http://www.omdbapi.com/?t=" + searchCriteria + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            var movie = response.data;
+            console.log("Title: " + movie.Title);
+            console.log("Year: " + movie.Year);
+            console.log("IMBD Rating: " + movie.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+            console.log("Country Produced: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Plot: " + movie.Plot);
+            console.log("Actors: " + movie.Actors);
+        }
+    );
+}
+function searchSpotify(searchCriteria = "The Sign") {
+    spotify.search({ type: 'track', query: searchCriteria }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            var songs = data.tracks.items;
+            for (var i = 0; i < songs.length; i++) {
+                var songs = data.tracks.items;
+                var artist = "Artist: " + songs[i].artists[0].name
+                var songName = "Song Name: " + songs[i].name;
+                var songPreview = "Song Preview: " + songs[i].external_urls.spotify;
+                var songAlbum = "Album: " + songs[i].album.name;
+                console.log(artist);
+                console.log(songName);
+                console.log(songPreview);
+                console.log(songAlbum);
+
+            }
+            // var combo = '\n' + artist + '\n' + songName + '\n' + songPreview + '\n' + songAlbum;
+            // fs.appendFile('log.txt', combo, function () {
+
+            // })
+        }
+    })
+
+}
